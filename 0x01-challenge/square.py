479ff53aeaad5154d63352f9b81f8f6ecb45c109
Drihmia <drihmia.redouane@gmail.com>
2024-03-14 00:01:25 +0000

    updates4

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,45 +3,31 @@
 square class
 """
 
-
-class square():
-    """ Documentation """
+class Square:
+    """Documentation"""
 
     width = 0
 
     def __init__(self, width=0):
-        """ Documentation """
-        self.__width = width
-
-    @property
-    def width(self):
-        """ Documentation """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if value < 0:
-            raise ValueError("width must be a positive number")
-        if type(value) not in ("int", "float"):
-            raise TypeError("with must be a number")
-        setattr(self, str(self.__width), int(value))
+        """Documentation"""
+        self.width = width
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.__width * self.__width
+        """Area of the square"""
+        return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        """ Documentation """
-        return (self.__width * 4)
+    def perimeter_of_my_square(self):
+        """Documentation"""
+        return self.width * 4
 
     def __str__(self):
-        """ Documentation """
-        return "{0}/{0}".format(self.__width)
+        """Documentation"""
+        return "{0}/{0}".format(self.width)
 
 
 if __name__ == "__main__":
-
-    s = square(width=12)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
+
