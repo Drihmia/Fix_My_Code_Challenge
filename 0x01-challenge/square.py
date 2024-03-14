bab500db9ac9dbe857ef6c05015eba0c52a07d19
Drihmia <drihmia.redouane@gmail.com>
2024-03-14 00:06:37 +0000

    updates8

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -9,7 +9,7 @@ class Square:
 
     width = 0
 
-    def __init__(self, width):
+    def __init__(self, width=width):
         """Documentation"""
         if width < 0:
             raise ValueError("Width must be a positive number")
@@ -32,7 +32,7 @@ class Square:
 
 if __name__ == "__main__":
 
-    s = Square(width=12)
+    s = Square()
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
