cf975fe79ee1fe2e6952d0e316a4d46ac0fa23ad
Drihmia <drihmia.redouane@gmail.com>
2024-03-17 20:30:00 +0000

    documented main funtion

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -32,8 +32,8 @@ class Square:
 
 
 if __name__ == "__main__":
-
-    s = Square(width=1, height=9)
+    """entry point to this module"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
