77b8a3dfb5cf3a5b815a8608e3bcccc1612a0e13
Drihmia <drihmia.redouane@gmail.com>
2024-03-18 23:20:34 +0000

    change the name of the perimeter

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -17,7 +17,7 @@ class Square():
         area = self.width * self.height
         return area
 
-    def permeter_of_my_square(self):
+    def permiter_of_my_square(self):
         """Documentation: the perimeter of the square"""
         perimeter = (self.width * 2) + (self.height * 2)
         return perimeter
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permeter_of_my_square())
+    print(s.permiter_of_my_square())
